6ac9c599c75f88a313ce75f28d634cab1c273934
EmilK102 <vip.elick@gmail.com>
2021-11-11 23:39:24 +0500

    dynamic obj

--- OOP_Class_and_Object/Source.cpp
+++ OOP_Class_and_Object/Source.cpp
@@ -2,12 +2,24 @@
 #include <clocale>
 
 class Point {
+private:
 	int x;
 	int y;
 public:
 	Point() {
 		this->x = 0;
 		this->y = 0;
+		printf("Point()\n");
+	}
+	Point(int x, int y) {
+		this->x = x;
+		this->y = y;
+		printf("Point(int x,int y)\n");
+	}
+	Point(const Point &p) {
+		this->x = p.x;
+		this->y = p.y;
+		printf("Point(const Point &p)\n");
 	}
 	void SetX(int x) {
 		this->x = x;
@@ -22,15 +34,25 @@ public:
 		return this->y;
 	}
 	~Point() {
-		printf("~Point()\n");
+		printf("~Point(%i,%i)\n",x,y);
 	}
 };
 
 class Point3D : public Point {
+private:
 	int z;
 public:
 	Point3D() {
 		this->z = 0;
+		printf("Point3D()\n");
+	}
+	Point3D(int z) {
+		this->z = z;
+		printf("Point3D(int z)\n");
+	}
+	Point3D(const Point3D &p) {
+		this->z = p.z;
+		printf("Point3D(const Point3D &p)\n");
 	}
 	void SetZ(int z) {
 		this->z = z;
@@ -39,17 +61,28 @@ public:
 		return this->z;
 	}
 	~Point3D() {
-		printf("~Point3D()\n");
+		printf("~Point3D(x,y,%i)\n",z);
 	}
 };
 
 int main() {
 	setlocale(LC_ALL, "");
 
-	Point3D p3d;
+	{
+		Point p1;
+		Point p2 = Point(1, 2);
+		Point p3 = Point(p2);
+	}
+	{
+		Point *p1 = new Point();
+		Point *p2 = new Point(1, 2);
+		Point *p3 = new Point(*p2);
 
-	p3d.SetY(5);
-	printf("Y: %i\n", p3d.GetY());
+		delete p1;
+		delete p2;
+		delete p3;
+	}
+
 
 	return 0;
 }
